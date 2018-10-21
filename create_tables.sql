5b4d706707f09da54dbd1da2537ac9112799f332
Michael Bauer <da1da2da3ofme@gmail.com>
2018-10-21 11:46:14 +1100

    added more db stuffs

--- create_tables.sql
+++ create_tables.sql
@@ -8,14 +8,18 @@ CREATE TABLE IF NOT EXISTS alexa_response (
 
 CREATE TABLE IF NOT EXISTS world_state (
 	id			SERIAL		PRIMARY KEY,
-	planetname	TEXT,
-	lookingat 	TEXT,
-	pressure 	FLOAT,
-	temperature FLOAT,
+	planetname	TEXT  DEFAULT "mars",
+	lookingat 	TEXT  DEFAULT "",
+	pressure 	FLOAT DEFAULT 0.0,
+	temperature FLOAT DEFAULT 0.0,
+	gravity FLOAT DEFAULT 0.0,
+	n2Level FLOAT DEFAULT 0.0,
+	co2Level FLOAT DEFAULT 0.0,
+	o2Level FLOAT DEFAULT 0.0,
 	isdaytime	BOOLEAN DEFAULT TRUE NOT NULL,
 	islighton	BOOLEAN DEFAULT TRUE NOT NULL,
 	isdrillon	BOOLEAN DEFAULT TRUE NOT NULL,
-	settings	TEXT,
-	action		TEXT,
+	settings	TEXT DEFAULT "",
+	action		TEXT DEFAULT "",
 	timestamp   DATETIME DEFAULT CURRENT_TIMESTAMP
 );
